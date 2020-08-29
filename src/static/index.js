94e5528da43b6a67e57abc1aaf8369cc951aa17b
zpskek <wjdghdwns0@gmail.com>
2020-08-29 16:41:26 +0900

    Caht with SocketIO

--- src/static/index.js
+++ src/static/index.js
@@ -1,6 +1,18 @@
 /* eslint-disable no-undef */
 const socket = io('/');
 
-socket.on('hello', () => console.log('Somebody Joind'));
+function sendMessage(message) {
+  socket.emit('newMessage', { message });
+  console.log(`You: ${message}`);
+}
 
-socket.emit('helloGuys');
+function setNickname(nickname) {
+  socket.emit('setNickname', { nickname });
+}
+
+function handleMessageNotif(data) {
+  const { message, nickname } = data;
+  console.log(`${nickname}: ${message}`);
+}
+
+socket.on('messageNotif', handleMessageNotif);
