316f9078240ef78eb78e4f9bc774246c7dfb5818
zpskek <wjdghdwns0@gmail.com>
2020-09-05 19:31:57 +0900

    Game Programming part one

--- src/socketController.js
+++ src/socketController.js
@@ -1,6 +1,12 @@
 import events from './events';
+import { chooseWord } from './words';
 
 let sockets = [];
+let inProgress = false;
+let word = null;
+let leader = null;
+
+const chooseLeader = () => sockets[Math.floor(Math.random() * sockets.length)];
 
 const socketController = (socket, io) => {
   const broadcast = (event, data) => socket.broadcast.emit(event, data);
@@ -8,15 +14,42 @@ const socketController = (socket, io) => {
   const sendPlayerUpdate = () =>
     superBroadcast(events.playerUpdate, { sockets });
 
+  const startGame = () => {
+    if (inProgress === false) {
+      inProgress = true;
+      leader = chooseLeader();
+      word = chooseWord();
+      setTimeout(() => {
+        superBroadcast(events.gameStarted);
+        io.to(leader.id).emit(events.leaderNotif, { word });
+      }, 2000);
+    }
+  };
+
+  const endGame = () => {
+    inProgress = false;
+    superBroadcast(events.gameEnded);
+  };
+
   socket.on(events.setNickname, ({ nickname }) => {
     socket.nickname = nickname;
     sockets.push({ id: socket.id, points: 0, nickname });
     broadcast(events.newUser, { nickname });
     sendPlayerUpdate();
+    if (sockets.length === 2) {
+      startGame();
+    }
   });
 
   socket.on(events.disconnect, () => {
     sockets = sockets.filter((aSocket) => aSocket.id != socket.id);
+    if (sockets.length === 1) {
+      endGame();
+    } else if (leader) {
+      if (leader.id === socket.id) {
+        endGame();
+      }
+    }
     broadcast(events.disconnected, { nickname: socket.nickname });
     sendPlayerUpdate();
   });
